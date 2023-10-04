e1482583db5f363682b3ffb5b66b0ba9ba502545
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-10-04 23:28:13 +0200

    added features

--- app.js
+++ app.js
@@ -51,6 +51,16 @@ e.preventDefault();
 // locate a specific section
 const id = e.currentTarget.getAttribute('href').slice(1);
 const element = document.getElementById(id);
+// calculate height
+const navHeight = navbar.getBoundingClientRect().height;
+const containerHeight = linksContainer.getBoundingClientRect().height;
+const fixedNav = navbar.clientList.contains('fixed-nav');
+let position = element.offsetTop - navHeight;
+window.scrollTo({
+    left: 0, 
+    top: position,
+});
+linksContainer.style.height = 0;
  });
 });
 
